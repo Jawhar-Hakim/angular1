db31c065bbde5c244beb3c20269486f876ded9d8
Jawhar-Hakim <jawharhakim2016@gmail.com>
2026-04-03 00:15:41 +0100

    03/04

--- src/app/evt-create-comp/evt-create-comp.component.ts
+++ src/app/evt-create-comp/evt-create-comp.component.ts
@@ -8,38 +8,58 @@ import { EvtService } from 'src/Services/evt.service';
   templateUrl: './evt-create-comp.component.html',
   styleUrls: ['./evt-create-comp.component.css']
 })
-export class EvtCreateCompComponent implements OnInit{
-  constructor(private dialogRef:MatDialogRef<EvtCreateCompComponent>,@Inject(MAT_DIALOG_DATA)data:any,private ES:EvtService){
-    if(data){
-      this.ES.getEventById(data).subscribe((E)=>{
-        this.form=new FormGroup({
-          Titre:new FormControl(E.Titre),
-          DateDebut:new FormControl(E.DateDebut),
-        })
-      })
-    }
-    else{
-      this.form=new FormGroup({
-      Titre:new  FormControl(null),
-      DateDebut:new FormControl(null),
-      DateFin:new FormControl(null),
-      Lieu:new FormControl(null)
-    })
-    }
-  }
-  form!:FormGroup
+export class EvtCreateCompComponent implements OnInit {
+  form: FormGroup;
   readonly range = new FormGroup({
     start: new FormControl<Date | null>(null),
     end: new FormControl<Date | null>(null),
   });
-  ngOnInit(){
-    
-  }
-  save() {
-        this.dialogRef.close(this.form.value);
 
+  constructor(
+    private dialogRef: MatDialogRef<EvtCreateCompComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private ES: EvtService
+  ) {
+    this.form = new FormGroup({
+      id: new FormControl(null),
+      Titre: new FormControl(null),
+      DateDebut: new FormControl(null),
+      DateFin: new FormControl(null),
+      Lieu: new FormControl(null),
+      Actions: new FormControl(null)
+    });
+
+    if (data) {
+      this.ES.getEventById(data).subscribe((E) => {
+        this.form.patchValue(E);
+        if (E.DateDebut || E.DateFin) {
+          this.range.patchValue({
+            start: E.DateDebut ? new Date(E.DateDebut as string) : null,
+            end: E.DateFin ? new Date(E.DateFin as string) : null
+          });
+        }
+      });
+    }
   }
+
+  ngOnInit() { }
+
+  save() {
+    const finalValue = {
+      ...this.form.value,
+      DateDebut: this.range.value.start,
+      DateFin: this.range.value.end
+    };
+    
+    // Remove null id to avoid issues with creation
+    if (finalValue.id === null) {
+      delete finalValue.id;
+    }
+    
+    this.dialogRef.close(finalValue);
+  }
+
   close() {
-        this.dialogRef.close();
+    this.dialogRef.close();
   }
 }
